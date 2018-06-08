8766b4932c79524c0f74ad4473142a9fc25964af
Cameron485 <cfindl15@caledonian.ac.uk>
2018-06-08 17:18:31 +0100

    Initial setup of redux and dependencies

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Platform, StatusBar } from 'react-native';
+import { TabNavigator, StackNavigator } from 'react-navigation';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
 export default class App extends React.Component {
   render() {
